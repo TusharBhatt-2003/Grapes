82a67ac37f6cf983e97adef3d023537ebb54c93e
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-09-17 01:24:06 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
     };
 
     return (
-        <div className='flex items-center justify-center w-full'>
+        <div className='flex items-center justify-center m-9'>
             <div className='mx-auto w-full max-w-lg bg-[#EFFFF2] rounded-xl p-10 border border-black/10'>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
