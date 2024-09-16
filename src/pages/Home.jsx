9d7e759738a1bd9a450ac3cb9bb9e425fba55fb9
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-09-17 00:51:07 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
-                                No posts available or please login
+                                Loading...Your posts...
                             </h1>
                         </div>
                     </div>
