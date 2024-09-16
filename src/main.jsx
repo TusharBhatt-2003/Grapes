3aac3b6e2f33ef8dbd50df5dcce4003233e72a97
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-09-17 00:10:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import { AuthLayout, Login,} from './components/index.js'
-import AddPost from "./pages/AddPost";
+import AddPost from "./pages/Addpost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
